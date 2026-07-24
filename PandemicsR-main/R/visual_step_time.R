5749e35f1327ea916b0429432c8de810ac230ff4
Victor Bernal <44974039+V-Bernal@users.noreply.github.com>
2026-07-24 13:45:51 +0200

    Add files via upload

--- PandemicsR-main/R/visual_step_time.R
+++ PandemicsR-main/R/visual_step_time.R
@@ -64,29 +64,3 @@ visual_step_time <- function(frac_mat, num_opinions){
     )
   }
 }
-
-
-
-# visual_step_time <- function(frac_mat, num_opinions, n){
-#
-#   levels_vec <- get_levels_vec(num_opinions)
-#   my_palette <- get_palette(num_opinions)
-#
-#   # Note: frac was recorded every n events. ticks are rescaled by mult by n
-#   plot.new()
-#   plot.window(xlim = c(1, nrow(frac_mat)), ylim = c(0, 1), xaxt = "n")  # suppress x-axis)
-#   axis(1, at = c(1: nrow(frac_mat)), labels = n*(1:nrow(frac_mat)))
-#   #plot.window(xlim = c(1, nrow(frac_mat)), ylim = c(0, 1))
-#   #axis(1)
-#   axis(2)
-#   box()
-#   title(xlab = "Time", ylab = "Voter Fraction")
-#
-#   for (k in 1:num_opinions) {
-#     lines(x = 1:nrow(frac_mat), y = frac_mat[,k ],
-#           lwd = 2, col = my_palette[k])
-#   }
-#
-#   # Update line plot for fraction of +1
-#   #Sys.sleep(0.05) # pause so you can see the update
-# }
